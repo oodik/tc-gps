720af1e9e52796bffaba4fa6115f854582767002
oodik <99489000+oodik@users.noreply.github.com>
2022-09-06 14:31:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ app.post("/api/arrive-here", (req, res) => {
       degrees = degrees + 270
     } 
     
-    degrees = degrees + (cotane(medirian/paraller))
+    degrees = degrees + (1/Math.tan(medirian/paraller))
       
     
     return(degrees)
